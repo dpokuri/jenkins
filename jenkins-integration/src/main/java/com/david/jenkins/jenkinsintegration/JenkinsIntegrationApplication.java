cb4573977c1a119a6f12378472dbad91f15b4b61
David Pokuri <TresmDisk@Davids-MacBook-Pro.local>
2018-04-04 12:22:43 +0530

    jenkins setup commit

---
+++ jenkins-integration/src/main/java/com/david/jenkins/jenkinsintegration/JenkinsIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.david.jenkins.jenkinsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsIntegrationApplication.class, args);
+	}
+}
