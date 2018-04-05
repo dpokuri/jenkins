0e0691638d572689c9887f8565b62eecc56288d7
David Pokuri <TresmDisk@Davids-MacBook-Pro.local>
2018-04-05 16:27:48 +0530

    jenkins files

---
+++ src/main/java/com/david/jenkins/jenkins/controller/JenkinsApplicationController.java
@@ -0,0 +1,16 @@
+package com.david.jenkins.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class JenkinsApplicationController {
+	
+	@GetMapping("/test")
+	public String message() {
+		return "Hello Jenkins";
+	}
+
+}
